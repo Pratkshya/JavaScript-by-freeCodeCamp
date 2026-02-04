e2052a88e87b79c71687ae66b7cae21752f144b4
Pratkshya <pratikshyaparajuli37@gmail.com>
2026-02-04 21:41:18 +0545

    use of variables

---
+++ triviaBot.js
@@ -0,0 +1,19 @@
+console.log("Hello! I'm your coding fun fact guide!");
+
+let botName = "Mikki";
+let botLocation = "Mars";
+let favoriteLanguage = "Marsense";
+
+console.log("My name is " + botName + " and I live on " + botLocation + ".");
+console.log("My favorite programming language is " + favoriteLanguage + ".");
+
+let codingFact = "I love to use " + favoriteLanguage + " as much as possible! (it's my language)";
+console.log(codingFact);
+
+codingFact = "Well I lied. I don't use " + favoriteLanguage + " that often.";
+console.log(codingFact);
+
+codingFact = "To be honest I use -.--. but not " + favoriteLanguage + ", it's my secret langauge.";
+console.log(codingFact);
+
+console.log("It was fun sharing these facts with you. Goodbye! - " + botName + " from " + botLocation + ".");
