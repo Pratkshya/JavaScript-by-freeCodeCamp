6ae377ad794faaa764fab25accbc06b3524161aa
Pratkshya <pratikshyaparajuli37@gmail.com>
2026-05-06 12:29:50 +0545

    use of increment and decrement

---
+++ debug-increment-and-decrement.js
@@ -0,0 +1,23 @@
+let counter = 10;
+console.log(`Counter is currently ${counter}`);
+
+let updatedCounter = ++counter;
+console.log(`Counter is now ${updatedCounter}`); //11
+
+let score = 8;
+console.log(`Score is currently ${score}`);
+
+let finalScore = score++;
+console.log(`Score is still ${finalScore}`); //8
+
+let coins = 3;
+console.log(`Coins is currently ${coins}`);
+
+let updatedCoins = --coins;
+console.log(`Coins is now ${updatedCoins}`); //2
+
+let health = 7;
+console.log(`Health is currently ${health}`);
+
+let newHealth = health--;
+console.log(`Health is still ${newHealth}`); //7
