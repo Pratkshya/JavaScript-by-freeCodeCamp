d80cab717ecfbb876cb033e7e0b119b5cc0395d7
Pratkshya <pratikshyaparajuli37@gmail.com>
2026-05-23 19:23:39 +0545

    use of switch

---
+++ card-counter-assistance.js
@@ -0,0 +1,38 @@
+let count = 0;
+function cardCounter(card){
+  let result = "";
+  switch(card){
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+    case 6:
+      count ++;
+      break;
+    case 7:
+    case 8:
+    case 9:
+      count;
+      break;
+    case 10:
+    case "J":
+    case "Q":
+    case "K":
+    case "A":
+      count --;
+      break;
+    default:
+      break;
+
+  }
+  if (count > 0){
+    result = `${count} Bet`;
+  }
+  else{
+    result = `${count} Hold`;
+  }
+  return result;
+}
+console.log(cardCounter(6));
+
+
