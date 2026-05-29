da60a240d36833fd1ecbbf0f173b5eec6c1147e1
Pratkshya <pratikshyaparajuli37@gmail.com>
2026-05-29 22:19:13 +0545

    use of array

---
+++ build-a-shopping-list.js
@@ -0,0 +1,41 @@
+console.log("Grocery shopping list");
+
+const shoppingList = [];
+
+console.log("It will be nice to have some fruit to eat.");
+
+shoppingList.push("Apples");
+
+function getShoppingListMsg(arr) {
+  return `Current Shopping List: ${arr}`;
+}
+
+console.log(getShoppingListMsg(shoppingList));
+
+shoppingList.push("Grapes"); //at the end of the array
+console.log(getShoppingListMsg(shoppingList));
+
+console.log("It looks like we need to get some cooking oil.");
+
+shoppingList.unshift("Vegetable Oil"); //at the beginning of the array
+console.log(getShoppingListMsg(shoppingList));
+
+shoppingList.push("Popcorn", "Beef Jerky", "Potato Chips");
+console.log(getShoppingListMsg(shoppingList));
+
+console.log("This looks like too much junk food.");
+
+shoppingList.pop(); //from the end of array
+console.log(getShoppingListMsg(shoppingList));
+
+console.log("It might be nice to get a dessert.");
+
+shoppingList.unshift("Chocolate Cake"); //adds at the beginning of the array
+console.log(getShoppingListMsg(shoppingList));
+
+console.log("On second thought, maybe we should be more health conscious.");
+
+shoppingList.shift(); //removes from the beginning
+shoppingList[0] = "Canola Oil";
+
+console.log(getShoppingListMsg(shoppingList));
