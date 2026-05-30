a5c90dd5a8ffd188711b2f219b999bcd7d754e1c
Pratkshya <pratikshyaparajuli37@gmail.com>
2026-05-30 23:00:11 +0545

    incomplete

---
+++ lunch-picker-program.js
@@ -0,0 +1,39 @@
+let lunches = [];
+
+function addLunchToEnd(arr, str){
+  arr.push(str);
+  console.log(`${str} added to the end of the lunch menu.`);
+  return arr;
+  
+}
+
+function addLunchToStart(arr,str){
+  arr.unshift(str);
+  console.log(`${str} added to the start of the lunch menu.`);
+  return arr;
+}
+
+function removeLastLunch(arr){
+  let poppedStr = arr.pop();
+  if(poppedStr){
+    console.log(`${poppedStr} removed from the end of the lunch menu.`);
+  }else{
+    console.log("No lunches to remove.");
+  }
+  return arr;
+}
+
+function removeFirstLunch(arr){
+  let poppedStr = arr.shift();
+  if(poppedStr){
+    console.log(`${poppedStr} removed from the start of the lunch menu.`);
+  }else{
+    console.log("No lunches to remove.");
+  }
+  return arr;
+}
+
+
+
+console.log(addLunchToEnd(["pizza", "tacoos","coke"], "burger"));
+console.log(removeLastLunch([]));
