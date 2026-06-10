c0907c662c8635d9e3877e034a99256fc58e99d5
Pratkshya <pratikshyaparajuli37@gmail.com>
2026-06-10 22:31:51 +0545

    added a comment

--- golf-score-translator.js
+++ golf-score-translator.js
@@ -1,6 +1,7 @@
 //list of names
 const names = ["Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Go Home!"];
 function golfScore(par, strokes){
+     //if else condition
      if(strokes === 1){
       return names[0];
      }
